72902b32e2a22395250cea58eabe6ae48bfbeeee
Tiago Taquelim <tiago.taquelim.ferreira@gmail.com>
2021-06-26 11:32:25 +0100

    revert color change

--- src/shared/styles.js
+++ src/shared/styles.js
@@ -11,7 +11,7 @@ export const background = {
 
 export const color = {
   // Palette
-  primary: '#ff0000', // coral
+  primary: '#FF4785', // coral
   secondary: '#1EA7FD', // ocean
   tertiary: '#DDDDDD',
 
